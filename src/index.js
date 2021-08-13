581b9e6c14342d7efa9900cd17f420f1ba75a269
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-08-13 19:17:46 -0300

    installed babel, middlewares, mongoose & nodemon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
 
+/* -------------------------------- Instance -------------------------------- */
 const app = express();
 
-const port = 4000;
+/* ---------------------------------- Port ---------------------------------- */
+app.set("port", process.env.PORT || 4000);
 
-app.listen(port, () => {
-  console.log("I'm using port: " + port);
+app.listen(app.get("port"), () => {
+  console.log("I'm using port: " + app.get("port"));
+  console.log(path.join(__dirname, "../public"));
+});
+/* ------------------------------- Middlewares ------------------------------ */
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+
+/* --------------------------------- Routes --------------------------------- */
+app.get("/", (req, res) => {
+  res.send("Hello from the server");
 });
